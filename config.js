f820cb87f5d13d6066757e3bcaed7d5af5c9a59b
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2021-06-24 12:16:12 -0400

    Initial setup is ready.

---
+++ config.js
@@ -0,0 +1,11 @@
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
+
+export const API = !publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.API_DEVELOPMENT
+  : publicRuntimeConfig.API_PRODUCTION;
+export const APP_NAME = publicRuntimeConfig.APP_NAME;
+export const DOMAIN = !publicRuntimeConfig.PRODUCTION
+  ? publicRuntimeConfig.DOMAIN_DEVELOPMENT
+  : publicRuntimeConfig.DOMAIN_PRODUCTION;
+export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
